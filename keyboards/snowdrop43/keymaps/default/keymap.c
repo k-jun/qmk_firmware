1c887179a0bdbb9bc81d850c3632ee478fbd9946
k-jun <keijun091221@gmail.com>
2024-06-22 18:51:41 +0900

    feat(output): done

--- keyboards/snowdrop43/keymaps/default/keymap.c
+++ keyboards/snowdrop43/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_BSPC, KC_SPC,  MO(2),   KC_LCMD,   KC_RCMD, MO(1),   KC_ENT
+                          KC_BSPC, KC_SPC,  MO(2),   CTL_T(KC_LNG1),   RCTL_T(KC_LNG2), MO(1),   KC_ENT
     ),
     [1] = LAYOUT(
         _______, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, _______,   _______, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, _______,
