d7c7daeea3a1f5fe7ebc50970ceff1dc511ee786
k-jun <keijun091221@gmail.com>
2024-11-16 11:33:41 +0900

    feat(all): config.h

--- keyboards/snowdrop43/config.h
+++ keyboards/snowdrop43/config.h
@@ -4,13 +4,15 @@
 #pragma once
 
 #define SPLIT_HAND_PIN D2
-#define USE_SERIAL
-#define SOFT_SERIAL_PIN D0
+// #define USE_SERIAL
+// #define SOFT_SERIAL_PIN D0
+#define USE_I2C
 
 #define SPLIT_POINTING_ENABLE
 #define POINTING_DEVICE_RIGHT
-#define TAPPING_TERM 150
+#define TAPPING_TERM 100
 
 #define PMW33XX_CPI 500
 #define PMW33XX_CS_PIN B6
 #define POINTING_DEVICE_INVERT_Y
+
